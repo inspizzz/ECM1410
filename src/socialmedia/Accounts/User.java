f148583fe4e6372cf17e29a301d344b648403d01
inspizzz <wiktor.wiejak:gmail.com>
2023-02-27 20:43:06 +0000

    added an id into the users file

--- src/socialmedia/Accounts/User.java
+++ src/socialmedia/Accounts/User.java
@@ -2,7 +2,9 @@ package socialmedia.Accounts;
 
 public class User {
     public static String name;
+    public static String id;
     public User() {
         name = "nametmp";
+        id = "some thing";
     }
 }
