6444c3e5b25559ecc29df1054a58b5e19853a663
inspizzz <wiktor.wiejak:gmail.com>
2023-02-28 01:07:50 +0000

    modified createAccounts and changeHandles, plan to make these more efficient tho

--- src/socialmedia/Accounts/User.java
+++ src/socialmedia/Accounts/User.java
@@ -4,10 +4,17 @@ public class User {
 
     private int userId;
     private String userHandle;
+    private String description;
 
     public User(int id, String handle) {
         this.userId = id;
         this.userHandle = handle;
+    }
+
+    public User(int id, String handle, String description) {
+        this.userId = id;
+        this.userHandle = handle;
+        this.description = description;
     }
 
     public int getId() {
@@ -18,11 +25,19 @@ public class User {
         return this.userHandle;
     }
 
+    public String getDescription() {
+        return this.description;
+    }
+
     public void setId(int id) {
         this.userId = id;
     }
 
     public void setHandle(String handle) {
         this.userHandle = handle;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
