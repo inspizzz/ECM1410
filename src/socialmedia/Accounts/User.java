b8aed7a3af708ee8e571f18cc9a47d86189ce21b
inspizzz <wiktor.wiejak:gmail.com>
2023-03-29 19:22:16 +0100

    added an interface for users to move the comments into it to make the User class cleaner

--- src/socialmedia/Accounts/User.java
+++ src/socialmedia/Accounts/User.java
@@ -1,5 +1,6 @@
 package socialmedia.Accounts;
 
+import socialmedia.Accounts.interfaces.MiniUser;
 import socialmedia.Posts.Comment;
 import socialmedia.Posts.Endorsement;
 import socialmedia.Posts.OriginalMessage;
@@ -7,7 +8,7 @@ import socialmedia.Posts.OriginalMessage;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class User implements Serializable {
+public class User implements MiniUser, Serializable {
     private int userId;
     private String userHandle;
     private String description;
@@ -16,158 +17,70 @@ public class User implements Serializable {
     private static final ArrayList<Comment> comments = new ArrayList<>();
     private static final ArrayList<Endorsement> endorsements = new ArrayList<>();
 
-    /**
-     * create an instance of a user using an id and handle
-     *
-     * @param id the id of user
-     * @param handle a string that references the user
-     */
     public User(int id, String handle) {
         this.userId = id;
         this.userHandle = handle;
     }
 
-    /**
-     * create an instance of a user using an id and handle and description
-     *
-     * @param id the id of user
-     * @param handle a string that references the user
-     * @param description a description
-     */
     public User(int id, String handle, String description) {
         this.userId = id;
         this.userHandle = handle;
         this.description = description;
     }
 
-    /**
-     * get the id of the user
-     *
-     * @return the id of the user
-     */
     public int getId() {
        return this.userId;
     }
 
-    /**
-     * get the handle of the user
-     *
-     * @return the handle of the user
-     */
     public String getHandle() {
         return this.userHandle;
     }
 
-    /**
-     * get the description of the user
-     *
-     * @return the description of the user
-     */
     public String getDescription() {
         return this.description;
     }
 
-    /**
-     * set the id of the user
-     *
-     * @param id the id to update the user with
-     */
     public void setId(int id) {
         this.userId = id;
     }
 
-    /**
-     *  set the handle of the user
-     *
-     * @param handle the handle to update the user with
-     */
     public void setHandle(String handle) {
         this.userHandle = handle;
     }
 
-    /**
-     * set the description of the user
-     *
-     * @param description the description to update the user with
-     */
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * add a message to the user, this would be called when this user
-     * has posted a message
-     *
-     * @param message added message instance
-     */
     public void addMessage(OriginalMessage message) {
         messages.add(message);
     }
 
-    /**
-     * get all messages that the user has posted
-     *
-     * @return the messages that the user has posted
-     */
     public ArrayList<OriginalMessage> getMessages() {
         return messages;
     }
 
-    /**
-     * add a comment to the user, this would be called when this user
-     * has posted a comment
-     *
-     * @param comment the comment that the user has posted
-     */
     public void addComment(Comment comment) {
         // add this to the users comments
         comments.add(comment);
     }
 
-    /**
-     * get all comments that the user has posted
-     *
-     * @return all the comments that the user has posted
-     */
     public ArrayList<Comment> getComments() {
         return comments;
     }
 
-    /**
-     *  add an endorsement to the user, this would be called when this user
-     *  has posted an endorsement
-     *
-     * @param endorsement the endorsement that the user has posted
-     */
     public void addEndorsement(Endorsement endorsement) {
         endorsements.add(endorsement);
     }
 
-    /**
-     * returns all endorsements that the user has posted
-     *
-     * @return an arraylist of all endorsements the user has posted
-     */
     public ArrayList<Endorsement> getEndorsements() {
         return endorsements;
     }
 
-    /**
-     * returns the number of posts that the user has posted in total
-     *
-     * @return the total number of messages, comments and endorsements
-     */
     public int getPostCount() {
         return messages.size() + comments.size() + endorsements.size();
     }
 
-    /**
-     * get the total number of endorsements that the users posts
-     * that can be endorsed contains, this is both messages and
-     * comments
-     *
-     * @return the number of endorsements the users posts contain
-     */
     public static int getEndorsementCount() {
         // posts can contain endorsements, count up all of them
         // check the messages and comments as endorsements cannot endorse endorsements
@@ -189,23 +102,14 @@ public class User implements Serializable {
         return count;
     }
 
-    /**
-     * clear the messages of the user
-     */
     public void clearMessages() {
         messages.clear();
     }
 
-    /**
-     * clear the users comments
-     */
     public void clearComments() {
         comments.clear();
     }
 
-    /**
-     * clear the users endoresements
-     */
     public void clearEndorsements() {
         endorsements.clear();
     }
---
+++ src/socialmedia/Accounts/interfaces/MiniUser.java
@@ -0,0 +1,130 @@
+package socialmedia.Accounts.interfaces;
+
+import socialmedia.Posts.Comment;
+import socialmedia.Posts.Endorsement;
+import socialmedia.Posts.OriginalMessage;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public interface MiniUser {
+    /**
+     * get the id of the user
+     *
+     * @return the id of the user
+     */
+    public int getId();
+
+    /**
+     * get the handle of the user
+     *
+     * @return the handle of the user
+     */
+    public String getHandle();
+
+    /**
+     * get the description of the user
+     *
+     * @return the description of the user
+     */
+    public String getDescription();
+
+    /**
+     * set the id of the user
+     *
+     * @param id the id to update the user with
+     */
+    public void setId(int id);
+
+    /**
+     *  set the handle of the user
+     *
+     * @param handle the handle to update the user with
+     */
+    public void setHandle(String handle);
+
+    /**
+     * set the description of the user
+     *
+     * @param description the description to update the user with
+     */
+    public void setDescription(String description);
+
+    /**
+     * add a message to the user, this would be called when this user
+     * has posted a message
+     *
+     * @param message added message instance
+     */
+    public void addMessage(OriginalMessage message);
+
+    /**
+     * get all messages that the user has posted
+     *
+     * @return the messages that the user has posted
+     */
+    public ArrayList<OriginalMessage> getMessages();
+
+    /**
+     * add a comment to the user, this would be called when this user
+     * has posted a comment
+     *
+     * @param comment the comment that the user has posted
+     */
+    public void addComment(Comment comment);
+
+    /**
+     * get all comments that the user has posted
+     *
+     * @return all the comments that the user has posted
+     */
+    public ArrayList<Comment> getComments();
+
+    /**
+     *  add an endorsement to the user, this would be called when this user
+     *  has posted an endorsement
+     *
+     * @param endorsement the endorsement that the user has posted
+     */
+    public void addEndorsement(Endorsement endorsement);
+
+    /**
+     * returns all endorsements that the user has posted
+     *
+     * @return an arraylist of all endorsements the user has posted
+     */
+    public ArrayList<Endorsement> getEndorsements();
+
+    /**
+     * returns the number of posts that the user has posted in total
+     *
+     * @return the total number of messages, comments and endorsements
+     */
+    public int getPostCount();
+
+    /**
+     * get the total number of endorsements that the users posts
+     * that can be endorsed contains, this is both messages and
+     * comments
+     *
+     * @return the number of endorsements the users posts contain
+     */
+    public static int getEndorsementCount() {
+        return 0;
+    }
+
+    /**
+     * clear the messages of the user
+     */
+    public void clearMessages();
+
+    /**
+     * clear the users comments
+     */
+    public void clearComments();
+
+    /**
+     * clear the users endoresements
+     */
+    public void clearEndorsements();
+}
